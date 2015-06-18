2b1c5b201a3d7856dcebb5521c30dfcd26c98acd
Max Minkoff <max@planetminkoff.com>
2015-06-18 08:36:20 -0400

    fixed problems

--- tests/unit/image/model-test.js
+++ tests/unit/image/model-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('image', 'Unit | Model | image', {
@@ -9,4 +10,15 @@ test('it exists', function(assert) {
   var model = this.subject();
   // var store = this.store();
   assert.ok(!!model);
+});
+
+test('it provides a thumbUrl', function(assert) {
+  var model = this.subject();
+  var baseUrl = 'http://foo.bar/baz/convert?crop=1,2,3,4';
+
+  Ember.run( function() {
+    model.set('url',baseUrl);
+  });
+
+  assert.equal(model.get('thumbUrl'),baseUrl + '&width=120');
 });
