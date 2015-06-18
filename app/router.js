4cf296e783c2f458a07afc06e501acccb720759c
Max Minkoff <max@planetminkoff.com>
2015-06-17 23:30:42 -0400

    Added new image route

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('my');
+  this.route('images', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
