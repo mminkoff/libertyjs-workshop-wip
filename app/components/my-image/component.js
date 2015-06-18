dfe5a786471a5bbcadb1437266cbfe5e2dc2bd9c
Max Minkoff <max@planetminkoff.com>
2015-06-18 09:35:43 -0400

    my-image component handles empty url, moved logic to component

--- app/components/my-image/component.js
+++ app/components/my-image/component.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['list-group-item']
+  classNames: ['list-group-item'],
+
+  url: Ember.computed('image.url','image.thumbUrl','thumb',function() {
+    if (this.get('thumb')) {
+      return this.get('image.thumbUrl');
+    } else {
+      return this.get('image.url');
+    }
+  })
 });
