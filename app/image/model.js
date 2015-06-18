d3a7aa68e60e0daf35a751f93131e65692897800
Max Minkoff <max@planetminkoff.com>
2015-06-18 00:26:52 -0400

    show image thumbnails

--- app/image/model.js
+++ app/image/model.js
@@ -4,5 +4,10 @@ export default DS.Model.extend({
   caption: DS.attr('string'),
   url: DS.attr('string'),
   height: DS.attr('number'),
-  width: DS.attr('number')
+  width: DS.attr('number'),
+
+  thumbUrl: Ember.computed('url',function() {
+    var u = this.get('url');
+    return u + '&width=120';
+  })
 });
