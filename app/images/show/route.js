fa9646bf0121dfa28ef0ddd9ece04624ee341c74
Max Minkoff <max@planetminkoff.com>
2015-06-18 23:15:14 -0400

    added ability to delete an image

--- app/images/show/route.js
+++ app/images/show/route.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    deleteImage: function() {
+      this.controller.get('model').destroyRecord();
+      this.transitionTo('images');
+    }
+  }
 });
